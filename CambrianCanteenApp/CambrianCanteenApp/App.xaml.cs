6a1a6a145bc705654d4b054ec25896d335f17afb
Gaurang Naik <A00250808@mycambrian.ca>
2023-06-02 23:20:18 -0400

    Initial Commit

---
+++ CambrianCanteenApp/CambrianCanteenApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CambrianCanteenApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CambrianCanteenApp/CambrianCanteenApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CambrianCanteenApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
